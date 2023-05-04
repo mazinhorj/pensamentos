b162fa3d3c64b770fb12fd122720b84245dc6131
mazinhorj <adm.osmarsilva@gmail.com>
2023-05-04 08:55:41 -0300

    update db

--- db/conn.js
+++ db/conn.js
@@ -1,13 +1,21 @@
-const {Sequelize} = require('sequelize')
+require('dotenv').config();
+const { Sequelize } = require('sequelize')
 
-const sequelize = new Sequelize('pensamentos', 'root', '', {
-    host: 'localhost',
-    dialect: 'mysql'
+
+const dbHost = process.env.DB_HOST;
+const dbUser = process.env.DB_USER;
+const dbPass = process.env.DB_PASS;
+const dbDb = process.env.DB_DB;
+const dbDialect = process.env.DB_DIALECT;
+
+const sequelize = new Sequelize(dbDb, dbUser, dbPass, {
+    host: dbHost,
+    dialect: dbDialect
 })
 
 try {
     sequelize.authenticate()
-    console.log('Conectado com sucesso.')
+    console.log('Conexão com banco de dados com sucesso.')
 
 } catch (error) {
     console.log(`A conexão com Banco de Dados falhou: ${err}`)
